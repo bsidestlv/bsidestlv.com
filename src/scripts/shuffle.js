fece08ebd17845129a21d2bc9c58e0ece426f661
Omer Cohen <git@omer.io>
2021-07-07 14:15:13 +0300

    Make agenda great again (#324)
    * desc > abs
    
    
    * js lint
    
    Signed-off-by: Omer <omer@bsidestlv.com>

--- src/scripts/shuffle.js
+++ src/scripts/shuffle.js
@@ -1,6 +1,6 @@
 // Shuffle
 document.querySelectorAll('.shuffle')
-  .forEach(function(listElt) {
+  .forEach(function (listElt) {
     if (listElt.children.length) {
       for (var i = listElt.children.length; i >= 0; i--) {
         listElt.appendChild(listElt.children[Math.random() * i | 0]);
