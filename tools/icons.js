488489a53346eee5c55820453d5f597827343657
Omer Cohen <git@omer.io>
2022-02-13 21:53:19 +0200

    fix seg fault?
    Signed-off-by: Omer Cohen <git@omer.io>

--- tools/icons.js
+++ tools/icons.js
@@ -1,29 +1,35 @@
-const { readFileSync, writeFileSync } = require('fs');
-const { sync: glob } = require('glob');
-const { Logger, LogLevel, colorEmojiConfig } = require('plop-logger');
+const { readFileSync, writeFileSync } = require("fs");
+const { sync: glob } = require("glob");
+const { Logger, LogLevel, colorEmojiConfig } = require("plop-logger");
 
 Logger.config = colorEmojiConfig;
-const logger = Logger.getLogger('icons');
+const logger = Logger.getLogger("icons");
 logger.level = LogLevel.All;
 
-let svgFiles = [`src/icons/*.svg`, `themes/devfest-theme-hugo-2020/src/icons/*.svg`];
-logger.info('SVG aggregation of', svgFiles);
+let svgFiles = [
+  `src/icons/*.svg`,
+  `themes/devfest-theme-hugo-2020/src/icons/*.svg`,
+];
+logger.info("SVG aggregation of", svgFiles);
 
-const data = svgFiles.flatMap(files => glob(files))
-  .map(file => {
-    const parts = file.split('/');
-    const id = parts[parts.length - 1].split('.')[0];
-    logger.debug('adding', id);
-    return readFileSync(file, 'utf8')
-      .replace(`<svg xmlns="http://www.w3.org/2000/svg"`, `\t<symbol id="${id}"`)
+const data = svgFiles
+  .flatMap((files) => glob(files))
+  .map((file) => {
+    const parts = file.split("/");
+    const id = parts[parts.length - 1].split(".")[0];
+    logger.debug("adding", id);
+    return readFileSync(file, "utf8")
+      .replace(
+        `<svg xmlns="http://www.w3.org/2000/svg"`,
+        `\t<symbol id="${id}"`
+      )
       .replace(`</svg>`, `\t</symbol>`);
-  }
-  );
+  });
 
 const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" class="visually-hidden">
-  ${data.join('\n')}
+  ${data.join("\n")}
 </svg>`;
 
-const file = 'static/icons.svg';
-logger.info('Generate SVG Icons', file);
-writeFileSync(file, svg, { flag: 'w' });
+const file = "static/icons.svg";
+logger.info("Generate SVG Icons", file);
+writeFileSync(file, svg, { flag: "w" });
--- tools/styles.js
+++ tools/styles.js
@@ -1,36 +1,41 @@
-const { writeFileSync } = require('fs');
-const path = require('path');
-const { sync: glob } = require('glob');
-const { Logger, LogLevel, colorEmojiConfig } = require('plop-logger');
-const sass = require('node-sass');
-const rimraf = require('rimraf');
+const { writeFileSync } = require("fs");
+const path = require("path");
+const { sync: glob } = require("glob");
+const { Logger, LogLevel, colorEmojiConfig } = require("plop-logger");
+const sass = require("node-sass");
+const rimraf = require("rimraf");
 
 Logger.config = colorEmojiConfig;
-const logger = Logger.getLogger('styles');
+const logger = Logger.getLogger("styles");
 logger.level = LogLevel.All;
 
 // Cleaning
 const cssFiles = `static/styles/*.css`;
-logger.info('Remove files', cssFiles);
+logger.info("Remove files", cssFiles);
 rimraf.sync(cssFiles);
 
 // Scss
 const scssFiles = `src/styles/*.scss`;
-logger.info('Compile CSS files', scssFiles);
+logger.info("Compile CSS files", scssFiles);
 const indentedSyntax = true;
-const outputStyle = 'expanded';
+const outputStyle = "expanded";
 
-glob(scssFiles)
-  .forEach(file => {
-    const { name } = path.parse(file);
-    const output = path.format({ dir: 'static/styles', name, ext: '.css' });
-    logger.debug(`Compile to ${output} to`, file);
+glob(scssFiles).forEach((file) => {
+  const { name } = path.parse(file);
+  const output = path.format({ dir: "static/styles", name, ext: ".css" });
+  logger.debug(`Compile to ${output} to`, file);
 
-    const result = sass.renderSync({ file, indentedSyntax, outputStyle, sourceMap: true, sourceMapEmbed: true });
-    if (result.css) {
-      writeFileSync(output, result.css, { flag: 'w' });
-    } else {
-      logger.error("Oops", result);
-      logger.dump(result);
-    }
+  const result = sass.renderSync({
+    file,
+    indentedSyntax,
+    outputStyle,
+    sourceMap: true,
+    sourceMapEmbed: true,
   });
+  if (result.css) {
+    writeFileSync(output, result.css, { flag: "w" });
+  } else {
+    logger.error("Oops", result);
+    logger.dump(result);
+  }
+});
