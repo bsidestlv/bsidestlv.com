2664e38e1908c03efa36b1ce0e59e6671c360bf0
Omer C <639682+omercnet@users.noreply.github.com>
2024-06-01 10:27:33 +0000

    make it faster

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
+import './modernizr';
 import './polyfills';
 import './shuffle';
-import './modernizr';
+
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i = 0; i < vidDefer.length; i++) {
+        if (vidDefer[i].getAttribute('data-src')) {
+            vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+        }
+    }
+}
+window.onload = init;
